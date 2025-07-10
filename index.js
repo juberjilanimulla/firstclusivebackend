a632a314eb531a1cfb443d947d924800ae5ba9a8
Juber_Mulla <juberjilanimulla@gmail.com>
2025-07-10 13:01:01 +0530

    this update for image deploy

--- index.js
+++ index.js
@@ -39,6 +39,8 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.json({ limit: "10mb" }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
 // Error handling middleware for JSON parsing errors
 app.use((err, req, res, next) => {
