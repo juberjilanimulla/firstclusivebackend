66ac60095e8affc05ef09aae96c4066af94338d3
Juber_Mulla <juberjilanimulla@gmail.com>
2025-07-29 16:25:14 +0530

    this is model for websitedesign

--- models/websitedesignmodel.js
+++ models/websitedesignmodel.js
@@ -2,11 +2,63 @@ import { Schema, model } from "mongoose";
 
 const websitedesignSchema = new Schema(
   {
+    paymentid: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "payment",
+    },
     name: String,
-    type: String,
-    paymentgateway: {
-      type: Boolean,
-      default: false,
+    email: String,
+    mobile: String,
+    address: String,
+    companyname: {
+      type: String,
+      default: "",
+    },
+    tagline: {
+      type: String,
+      default: "",
+    },
+    whichindustryareyou: {
+      type: String,
+      default: "",
+    },
+    businessdo: {
+      type: String,
+      default: "",
+    },
+    maincustomers: {
+      type: String,
+      default: "",
+    },
+    websitetofeel: {
+      type: String,
+      default: "",
+    },
+    coloursprefer: {
+      type: String,
+      default: "",
+    },
+    coloursavoid: {
+      type: String,
+      default: "",
+    },
+    referencesites: {
+      type: String,
+      default: "",
+    },
+    specificidea: {
+      type: String,
+      default: "",
+    },
+    uploadimage: [
+      {
+        type: String,
+        default: "",
+      },
+    ],
+    likeustoknow: {
+      type: String,
+      default: "",
     },
   },
 
